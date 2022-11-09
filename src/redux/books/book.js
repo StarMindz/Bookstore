20cb61f52b58c939417db071fa00eb8526970a63
StarMindz <64619873+StarMindz@users.noreply.github.com>
2022-11-09 11:27:33 +0100

    Add redux setup

--- src/redux/books/book.js
+++ src/redux/books/book.js
@@ -24,7 +24,7 @@ const bookReducer = (state = InitialState, action) => {
       return state.filter((item) => { return item.id !== action.payload.id; });
     default:
       return state;
-  };
+  }
 };
 
 export default bookReducer;
