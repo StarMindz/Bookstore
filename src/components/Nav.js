c6b4b801e9fc1dae0623114f275b5b5edfaeb959
starmindz <64619873+StarMindz@users.noreply.github.com>
2022-11-12 21:55:16 +0100

    Update styling for Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,49 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { FaUserAlt } from 'react-icons/fa';
 
-const Nav = () => {
-  const nav = (
-    <div className="navBar">
-      <Link className="home" to="/">Books</Link>
-      <Link className="categories" to="/categories">Categories</Link>
-    </div>
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Books',
+    },
+    {
+      id: 2,
+      path: '/categories',
+      text: 'Categories',
+    },
+  ];
+
+  return (
+    <nav className="flex justify-between py-2 bg-gray-00 items-center border-b border-b-gray-200">
+      <div className="flex space-x-14 items-center mx-5">
+
+        <h1 className="text-3xl font-bold text-blue-700">Bookstore CMS</h1>
+        <ul className="flex space-x-14 text-2l font-thin">
+          {links.map(({ id, path, text }) => (
+            <li key={id}>
+              <NavLink
+                end
+                to={path}
+                className="hover:text-zinc-50 active:text-gray-400"
+              >
+                {text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+
+        <div className="bg-white p-4 rounded-full mx-5">
+          <FaUserAlt className="text-blue-600" />
+        </div>
+      </div>
+    </nav>
   );
-  return nav;
 };
+
 
 export default Nav;
